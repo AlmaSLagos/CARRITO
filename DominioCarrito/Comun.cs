4c2130229d565ae135a277ba3c7a1797c6718094
Alma S Lagos <almaslagos@gmail.com>
2020-08-06 18:05:55 -0300

    Cliente (inheritance class)

---
+++ DominioCarrito/Comun.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominioCarrito
+{
+    class Comun:Cliente
+    {
+        private string nombre;
+        private string cedula;
+        private string celular;
+
+        #region Property
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public string Cedula { get { return cedula; } set { cedula = value; } }
+        public string Celular { get { return celular; } set { celular = value; } }
+        #endregion
+        #region HerenciaConstructor
+        public Comun(string mail, string direccion, string nombreUser, int departamento, DateTime fechaRegistro, string password, string nombre, string cedula, string celular) : base(mail, direccion, nombreUser, departamento, fechaRegistro, password)
+        {
+            Nombre = nombre;
+            Cedula = cedula;
+            Celular = celular;
+        }
+        #endregion
+        public override string ToString()
+        {
+            return base.ToString() + " Nombre del Cliente: " + Nombre + " Cedula: " + Cedula + " Celular: " + Celular;
+        }
+    }
+}
---
+++ DominioCarrito/Empresa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominioCarrito
+{
+    class Empresa:Cliente
+    {
+        private string nombreEmpresa;
+        private string razonSocial;
+        private string rut;
+
+        #region Propertys
+        public string NombreEmpresa { get { return nombreEmpresa; } set { nombreEmpresa = value; } }
+        public string RazonSocial { get { return razonSocial; } set { razonSocial = value; } }
+        public string Rut { get { return rut; } set { rut = value; } }
+        #endregion
+        #region HerenciaConstructor
+        public Empresa(string mail, string direccion, string nombreUser, int departamento, DateTime fechaRegistro, string password, string nombreEmpresa, string razonSocial, string rut) : base(mail, direccion, nombreUser, departamento, fechaRegistro, password)
+        {
+            NombreEmpresa = nombreEmpresa;
+            RazonSocial = razonSocial;
+            Rut = rut;
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return base.ToString() + " Nombre de la Empresa: " + NombreEmpresa + " Razon Social: " + RazonSocial + " Rut: " + Rut;
+        }
+    }
+}
