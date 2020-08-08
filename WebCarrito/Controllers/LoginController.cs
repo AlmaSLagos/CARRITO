2fa65558035ebdc16673c93c2be9dec1aebb584d
Alma S Lagos <almaslagos@gmail.com>
2020-08-08 14:27:29 -0300

    Login I

--- WebCarrito/Controllers/LoginController.cs
+++ WebCarrito/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DominioCarrito;
 
 namespace WebCarrito.Controllers
 {
@@ -13,5 +14,15 @@ namespace WebCarrito.Controllers
         {
             return View();
         }
+        /*
+        public ActionResult Login(string mail, string password)
+        {
+            if (mail != "" && password != "")
+            {
+                ViewBag.Mensaje = Sistema.Instancia.Login(mail, password);
+            }
+            return View("Index");
+        }
+        */
     }
 }
